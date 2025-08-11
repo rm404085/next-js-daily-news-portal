8aae334b6724fa37c20dc52c06502e528f25e54b
rm40405 <rm404085@gmail.com>
2025-08-11 19:03:11 +0600

    first commit

---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+       <header>
+        <nav>
+            {/* logo */}
+
+            <div>
+                <Link href="/" >Daily News</Link>
+            </div>
+
+            {/* destop menu */}
+
+
+            {/* login */}
+        </nav>
+       </header>
+    );
+};
+
+export default Navbar;
